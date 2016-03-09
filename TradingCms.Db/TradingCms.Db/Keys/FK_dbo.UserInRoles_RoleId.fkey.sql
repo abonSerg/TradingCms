bf19335d326a3afbc55194081e49f662d3f7c2a2
Vtaliy Svishevskiy <studCBDevProject1@gmail.com>
2016-03-09 21:26:46 +0300

    TP-9 current name changes

---
+++ TradingCms.Db/TradingCms.Db/Keys/FK_dbo.UserInRoles_RoleId.fkey.sql
@@ -0,0 +1,4 @@
+﻿alter table [dbo.UserInRoles]
+	add Constraint [FK_dbo.Roles_RoleId] FOREIGN KEY ([Role_Id]) REFERENCES [dbo].[Roles] ([Id]) ON DELETE CASCADE
+	
+go
---
+++ TradingCms.Db/TradingCms.Db/Tables/UsersInRoles.table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UsersInRoles]
+(
+	[User_Id] NvarCHAR(128) NOT NUll, 
+    [Role_Id] NvarCHAR(128)NOT NUll,
+	
+	
+    
+)
