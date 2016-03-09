b2360ea253464a984d7edc60dc4dd080952f854c
Yevgen Kurapov <Evgentus89@gmail.com>
2016-03-09 16:00:41 +0200

    TP-3

---
+++ TradingCms.Db/TradingCms.Db/Tables/FeedBacks.table.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[FeedBacks]
+(
+	[Id] INT NOT NULL,
+	[ReplyFeedBackId] INT NULL,
+	[CreatorId] NVARCHAR (128) NOT NULL,
+	[CreateDate] DATETIME NOT NULL, 
+    [IsActive] BIT NOT NULL DEFAULT 1, 
+    [IsModerated] BIT NOT NULL DEFAULT 0, 
+	[ModerateDate] DATETIME NULL, 
+    [Rate] SMALLINT CHECK ( rate in (1, 2, 3, 4, 5) ) NULL, 
+    [Advantages] NVARCHAR(1024) NULL, 
+    [Disadvantages] NVARCHAR(1024) NULL, 
+    [Text] NVARCHAR(MAX) NULL, 
+    [ProductId] INT NOT NULL
+)
---
+++ TradingCms.Db/TradingCms.Db/Tables/Keys/FK_FeedBacks_ProductId.fkey.sql
@@ -0,0 +1,2 @@
+﻿ALTER TABLE [dbo].[FeedBacks]
+	ADD CONSTRAINT [FK_FeedBacks_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [Products]([Id]);
