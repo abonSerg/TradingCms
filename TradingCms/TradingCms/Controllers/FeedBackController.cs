0f22d63413f6641dbe49f5b5339d8b95f44f4f3c
Yevgen Kurapov <Evgentus89@gmail.com>
2016-03-09 16:43:13 +0200

    TP-12 AS PO I want to see feedbacks in the product page. init projects structure and add nHibernate mapping class to FeedBacks

---
+++ TradingCms/TradingCms/Controllers/FeedBackController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Http;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using TradingCms.Data.Entities;
+using TradingCms.Data.SqlServer.Mapping;
+
+namespace TradingCms.Controllers
+{
+    public class FeedBackController : ApiController
+    {
+        // GET api/FeedBack
+        public FeedBack Get(int id)
+        {
+            var sessionFactory = Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql2008
+                  .ConnectionString(c => c.FromConnectionStringWithKey("defaultConnectionString")))
+                  .CurrentSessionContext("web")
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<FeedBackMap>())
+                .BuildSessionFactory();
+
+
+            var session = sessionFactory.OpenSession();
+            using (var transaction = session.BeginTransaction())
+            {
+                var res = session.Get(typeof (FeedBack), 1);
+                return res as FeedBack;
+            }
+        }
+    }
+}
