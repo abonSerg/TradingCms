3cda73c33bd876398cb85f35473f6a5f7f730dc0
Kurapov Yevgen <Evgentus89@gmail.com>
2016-03-15 21:36:29 +0200

    TP-24 added relational table for product with translating description

--- TradingCms.Db/TradingCms.Db/Tables/Products.table.sql
+++ TradingCms.Db/TradingCms.Db/Tables/Products.table.sql
@@ -2,11 +2,8 @@
 (
 	[Id] INT IDENTITY(1,1) NOT NULL,
 	[CategoryId] INT NOT NULL, 
-    [Name] VARCHAR(128) NOT NULL, 
     [Price] MONEY NOT NULL, 
 	[IsAvailable] BIT NOT NULL DEFAULT 1,
-    [DescriptionUk] VARCHAR(256) NULL, 
-	[DescriptionRu] VARCHAR(256) NULL, 
     [CreateDate] DATETIME NOT NULL
 )
 
---
+++ TradingCms.Db/TradingCms.Db/Tables/Products_tr.table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Products_tr]
+(
+	[Id] INT NOT NULL,
+	[Languagecode] VARCHAR(2) NOT NULL,
+    [Name] VARCHAR(256) NOT NULL,
+	[Description] VARCHAR(MAX) NULL
+)
+
