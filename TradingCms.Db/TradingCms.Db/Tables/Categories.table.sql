a73c355ab9f6e46948f5e9ab125618fbed909247
Kurapov Yevgen <Evgentus89@gmail.com>
2016-03-15 22:06:33 +0200

    TP-24 added relational table for categories with translating description

--- TradingCms.Db/TradingCms.Db/Tables/Categories.table.sql
+++ TradingCms.Db/TradingCms.Db/Tables/Categories.table.sql
@@ -1,7 +1,5 @@
 ﻿CREATE TABLE [dbo].[Categories]
 (
 	[Id] INT IDENTITY(1,1) NOT NULL, 
-    [TopId] INT NULL, 
-    [Name] NVARCHAR(32) NOT NULL, 
-    [Description] NVARCHAR(128) NULL
+    [TopId] INT NULL
 )
