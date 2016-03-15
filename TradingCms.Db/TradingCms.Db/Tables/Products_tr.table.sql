c1e207934567398980e5ab654b8341d842d58190
Kurapov Yevgen <Evgentus89@gmail.com>
2016-03-15 21:48:13 +0200

    TP-24 varchar -> nvarchar

--- TradingCms.Db/TradingCms.Db/Tables/Products_tr.table.sql
+++ TradingCms.Db/TradingCms.Db/Tables/Products_tr.table.sql
@@ -2,7 +2,7 @@
 (
 	[Id] INT NOT NULL,
 	[Languagecode] VARCHAR(2) NOT NULL,
-    [Name] VARCHAR(256) NOT NULL,
-	[Description] VARCHAR(MAX) NULL
+    [Name] NVARCHAR(256) NOT NULL,
+	[Description] NVARCHAR(MAX) NULL
 )
 
