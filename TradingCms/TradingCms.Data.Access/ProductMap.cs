7099e1926e940623357ecaf4b7532ca68455e76f
Bondar Sergiy <bob.zvir@mail.ru>
2016-03-16 14:44:46 +0200

    TP-10 rewrite code for new db structures

--- TradingCms/TradingCms.Data.Access/ProductMap.cs
+++ TradingCms/TradingCms.Data.Access/ProductMap.cs
@@ -7,15 +7,14 @@ namespace TradingCms.Data.Access
         public ProductMap()
         {
             Id(x => x.Id);
-            Map(x => x.Name).Not.Nullable();
             Map(x => x.Price).Not.Nullable();
             Map(x => x.IsAvailable).Not.Nullable();
-            Map(x => x.DescriptionUk).Nullable();
-            Map(x => x.DescriptionRu).Nullable();
             Map(x => x.CreateDate).Not.Nullable();
 
             Map(x => x.CategoryId).Not.Nullable();
-            //References(x => x.Category, "Category").Not.Nullable();
+            References(x => x.Category, "Categories").Not.Nullable();
+
+            HasMany(x => x.ProductTranslations).Inverse();
 
             Table("Products");
         }
