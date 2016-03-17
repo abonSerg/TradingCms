4b1c25e00659e1fd6f3b56f499d04816ed66575a
Vtaliy Svishevskiy <studCBDevProject1@gmail.com>
2016-03-17 19:17:25 +0300

    TP-20 Script for TestDatabase

--- TradingCms.Db/TradingCms.Db/Table/Keys/FK_dbo.UserInRoles_Category_Id.sql
+++
@@ -1,3 +0,0 @@
-﻿alter table [dbo.UserInRoles]
-add	CONSTRAINT [FK_dbo.Category_CategoryId] FOREIGN KEY ([Category_Id]) REFERENCES [dbo].[Category] ([Id]) ON DELETE CASCADE	
-go
--- TradingCms.Db/TradingCms.Db/Table/UsersInRoles.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[UsersInRoles]
-(
-	[User_Id] NvarCHAR(128) NOT NUll, 
-    [Role_Id] NvarCHAR(128)NOT NUll,
-	
-	
-    
-)
