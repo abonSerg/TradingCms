e9103bd6527f35083fe0b11c0180d4906d47b4ef
Bondar Sergiy <bob.zvir@mail.ru>
2016-03-17 22:01:52 +0200

    TP-10,11 Fix some bugs

--- TradingCms/TradingCms/Controllers/CategoryController.cs
+++ TradingCms/TradingCms/Controllers/CategoryController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Linq;
 using System.Net;
 using System.Web.Mvc;
-using NHibernate.Exceptions;
+//using NHibernate.Exceptions;
 using TradingCms.Data;
 using TradingCms.Data.Access;
 
@@ -34,104 +34,104 @@ namespace TradingCms.Controllers
         }
 
         // GET: Category/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
 
         // POST: Category/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(Category category)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    CategoryRepository.Add(category);
-                    CategoryRepository.Flush();
-                    return RedirectToAction("Index");
-                }
-            }
-            catch (DataException)
-            {
-                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
-            }
-            return View(category);
-        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(Category category)
+        //{
+        //    try
+        //    {
+        //        if (ModelState.IsValid)
+        //        {
+        //            CategoryRepository.Add(category);
+        //            CategoryRepository.Flush();
+        //            return RedirectToAction("Index");
+        //        }
+        //    }
+        //    catch (DataException)
+        //    {
+        //        ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+        //    }
+        //    return View(category);
+        //}
 
         // GET: Category/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            var category = CategoryRepository.Find(id);
-            if (category == null)
-            {
-                return HttpNotFound();
-            }
-            return View(category);
-        }
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    var category = CategoryRepository.Find(id);
+        //    if (category == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(category);
+        //}
 
         // POST: Category/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(Category category)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    CategoryRepository.Update(category);
-                    CategoryRepository.Flush();
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(Category category)
+        //{
+        //    try
+        //    {
+        //        if (ModelState.IsValid)
+        //        {
+        //            CategoryRepository.Update(category);
+        //            CategoryRepository.Flush();
 
-                    return RedirectToAction("Index");
-                }
-            }
-            catch (DataException)
-            {
-                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
-            }
-            return View(category);
-        }
+        //            return RedirectToAction("Index");
+        //        }
+        //    }
+        //    catch (DataException)
+        //    {
+        //        ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+        //    }
+        //    return View(category);
+        //}
 
         // GET: Category/Delete/5
-        public ActionResult Delete(int? id, bool? saveChangesError = false)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            if (saveChangesError.GetValueOrDefault())
-            {
-                ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
-            }
-            var category = CategoryRepository.Find(id);
-            if (category == null)
-            {
-                return HttpNotFound();
-            }
-            return View(category);
-        }
+        //public ActionResult Delete(int? id, bool? saveChangesError = false)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    if (saveChangesError.GetValueOrDefault())
+        //    {
+        //        ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
+        //    }
+        //    var category = CategoryRepository.Find(id);
+        //    if (category == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(category);
+        //}
 
         // POST: Category/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            try
-            {
-                var category = CategoryRepository.Find(id);
-                CategoryRepository.Delete(category);
-                CategoryRepository.Flush();
-            }
-            catch (DataException)
-            {
-                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
-            }
-            return RedirectToAction("Index");
-        }
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    try
+        //    {
+        //        var category = CategoryRepository.Find(id);
+        //        CategoryRepository.Delete(category);
+        //        CategoryRepository.Flush();
+        //    }
+        //    catch (DataException)
+        //    {
+        //        return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+        //    }
+        //    return RedirectToAction("Index");
+        //}
     }
 }
