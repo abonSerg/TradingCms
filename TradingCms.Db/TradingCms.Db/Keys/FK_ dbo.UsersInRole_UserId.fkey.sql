f6ef236a14531aace398fa0aee0b0b5fbbe6f75d
Vtaliy Svishevskiy <studCBDevProject1@gmail.com>
2016-03-17 19:53:21 +0300

    TP- 20 Change

--- TradingCms.Db/TradingCms.Db/Keys/FK_ dbo.UsersInRole_UserId.fkey.sql
+++
@@ -1,5 +0,0 @@
-﻿alter table [dbo.UserInRoles]
-	
-	add CONSTRAINT [FK_dbo.Users_UserId] FOREIGN KEY ([User_Id]) REFERENCES [dbo].[Users] ([Id]) ON DELETE CASCADE
-go
-
--- TradingCms.Db/TradingCms.Db/Keys/PK_dbo.UserInRoles.pkey.sql
+++
@@ -1,4 +0,0 @@
-﻿alter table [dbo.UserInRoles]
-	add CONSTRAINT [PK_dbo.UserInRoles] PRIMARY KEY CLUSTERED ([User_Id] ASC, [Role_Id] ASC)
-	
-go
