20bec57c3aef91ec54e9b819cc3901973f2ec34b
Bondar Sergiy <bob.zvir@mail.ru>
2016-03-19 09:39:41 +0200

    TP-10,11 Added tests

---
+++ TradingCms/TradingCms.Tests/Controllers/CategoryControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TradingCms.Controllers;
+
+namespace TradingCms.Tests.Controllers
+{
+    [TestClass]
+    public class CategoryControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            CategoryController categoryController = new CategoryController();
+
+            ViewResult result = categoryController.Index() as ViewResult;
+
+            Assert.IsNotNull(result.Model);
+        }
+
+        [TestMethod]
+        public void Details()
+        {
+            CategoryController categoryController = new CategoryController();
+
+            ViewResult result = categoryController.Details(1) as ViewResult;
+
+            Assert.IsNotNull(result.Model);
+        }
+    }
+}
---
+++ TradingCms/TradingCms.Tests/Controllers/ProductControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TradingCms.Controllers;
+
+namespace TradingCms.Tests.Controllers
+{
+    [TestClass]
+    public class ProductControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            ProductController productController = new ProductController();
+
+            ViewResult result = productController.Index() as ViewResult;
+
+            Assert.IsNotNull(result.Model);
+        }
+
+        [TestMethod]
+        public void Details()
+        {
+            ProductController productController = new ProductController();
+
+            ViewResult result = productController.Details(1) as ViewResult;
+
+            Assert.IsNotNull(result.Model);
+        }
+    }
+}
