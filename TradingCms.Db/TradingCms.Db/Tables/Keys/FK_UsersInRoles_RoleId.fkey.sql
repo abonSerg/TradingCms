618496a1e24d9c8e420f9bcb9c0b61190472f9ac
Vtaliy Svishevskiy <studCBDevProject1@gmail.com>
2016-03-23 19:17:51 +0300

    TP-9 sp3

--- TradingCms.Db/TradingCms.Db/Tables/Keys/FK_UsersInRoles_RoleId.fkey.sql
+++ TradingCms.Db/TradingCms.Db/Tables/Keys/FK_UsersInRoles_RoleId.fkey.sql
@@ -1,3 +1,3 @@
-﻿alter table [dbo].[UsersInRoles]
+﻿ALTER TABLE [dbo].[UsersInRoles]
 	add Constraint [FK_UsersInRoles_RoleId] FOREIGN KEY ([Role_Id]) REFERENCES [dbo].[Roles] ([Id]) ON DELETE CASCADE
 	
