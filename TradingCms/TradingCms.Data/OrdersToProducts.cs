1bd613b5cabf1e51ab03523bcfa10991110da5fc
Bondar Sergiy <bob.zvir@mail.ru>
2016-04-01 19:01:08 +0300

    TP-32 FIX

--- TradingCms/TradingCms.Data/OrdersToProducts.cs
+++ TradingCms/TradingCms.Data/OrdersToProducts.cs
@@ -5,6 +5,6 @@ namespace TradingCms.Data
     {
         public virtual Order Order { get; set; }
         public virtual Product Product { get; set; }
-        public virtual int Price { get; set; }
+        public virtual decimal Price { get; set; }
     }
 }
