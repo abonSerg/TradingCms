84a1a55d4aa88de9f3e33651bafad05908d500ed
Bondar Sergiy <bob.zvir@mail.ru>
2016-04-03 21:20:09 +0300

    TP-2 Added UserLogins and UserClaims tables

---
+++ TradingCms.Db/TradingCms.Db/Tables/Keys/PK_UserLogins_UserId_LoginProvider_ProviderKey.pkey.sql
@@ -0,0 +1,8 @@
+﻿ALTER TABLE [dbo].[UserLogins] ADD  CONSTRAINT [PK_UserLogins_UserId_LoginProvider_ProviderKey] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC, 
+	[LoginProvider] ASC, 
+	[ProviderKey] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+GO
+	
