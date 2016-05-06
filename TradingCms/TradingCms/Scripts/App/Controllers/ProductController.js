b7e55440f62f7db4d7f4a24b35955aecde5652a0
Yevgen Kurapov <Evgentus89@gmail.com>
2016-05-06 10:45:15 +0300

    tp-45 implementation angular and display top products

---
+++ TradingCms/TradingCms/Scripts/App/Controllers/ProductController.js
@@ -0,0 +1,10 @@
+﻿angular.module('productsApp', [])
+    .controller('productsCtrl', function ($scope, $http) {
+        $scope.getTopProducts = function() {
+            $http.get("/api/Product/GetTopProducts/10").success(function (data) {
+                $scope.topProducts = data;
+            }).error(function () {
+                alert("Oops... something went wrong");
+            });
+        }
+    });
